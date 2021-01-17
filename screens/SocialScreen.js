7813bfa6a8720ea961eb8a9bb57613d274213552
justin-curtis-lu <juclu@ucdavis.edu>
2021-01-16 21:03:30 -0800

    Update SocialScreen.js

--- screens/SocialScreen.js
+++ screens/SocialScreen.js
@@ -50,7 +50,7 @@
 // }
 
 import React, { useState, useEffect, Component } from 'react';
-import { Text, View, TouchableOpacity, ActivityIndicator, SafeAreaView, FlatList, ScrollView } from 'react-native';
+import { Text, View, TouchableOpacity, ActivityIndicator, SafeAreaView, FlatList, ScrollView, StyleSheet } from 'react-native';
 import * as SMS from 'expo-sms';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
@@ -162,12 +162,10 @@ export default function SocialScreen() {
                     friends.push(i);
                 }
             }  
-
             setFriends(friends);
             console.log(friends);
             setLoading(false);
       });
-
       // Unsubscribe from events when no longer in use
       return () => subscriber();
   }, []);
@@ -176,74 +174,75 @@ export default function SocialScreen() {
       return <ActivityIndicator />;
   }
 
-
-
-
-
-
-
-    function handleItemClick({index}) {
-        console.log(index);
-    };
-
-    function handleInnerItemClick({innerIndex, item, itemIndex}) {
-        console.log(innerIndex);
-    };
-
-
-
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '5%'
+        }}
+      />
+    )
+  }
 
     return (
-
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , marginTop: 120, backgroundColor: "#3b5998"}}>
-        <FontAwesome5 name="user-friends" size={30} color="black" />
-       
-        <View style={{flexDirection: 'row'}}>
-            <Text style={{ fontSize: 30, color: 'black' }}>Friends List ({friends.length}) </Text> 
-            <Ionicons name="person-add" size={24} color="black" />
-        </View>
-        <Text style={{ fontSize: 18, color: 'white' }}> </Text> 
-        <Text style={{ fontSize: 18, color: 'white' }}>Remind your friends to stay healthy</Text>
-        <Text style={{ fontSize: 18, color: 'white' }}> </Text> 
-        <Text style={{ fontSize: 14, color: 'white' }}>Eg: Drink more water today!</Text>
-        <Text style={{ fontSize: 14, color: 'white' }}> </Text> 
-        <SafeAreaView>
-            <FlatList
-                data={friends}
-                renderItem={({item}) => (
-                    <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: "row" }}>
-                        <Text>Friend: {item}</Text>
-                        <View style={{flexDirection: 'row'}}>
-                            <FontAwesome.Button onPress={() => SMS.sendSMSAsync(
-                                ['6504779097'],
-                                'Drink some more water!',)}
-                            style={{ backgroundColor: '#3b5998', flexDirection: "row"}}>
-                                <Ionicons name="chatbubble-ellipses-outline" size={20} color="black" />
-                            </FontAwesome.Button>
-                         </View>
-                    </View>
-                )}
-            />
-        </SafeAreaView>
-        
-        {/* <ExpandableListView
-            //ExpandableListViewStyles={{borderTopWidth:10}} // styles to expandable listview
-            renderInnerItemSeparator={true} // true or false, render separator between inner items
-            renderItemSeparator={true} // true or false, render separator between Items
-            //itemContainerStyle={{color:'white'}} // add your styles to all item container of your list
-            //itemLabelStyle={{color:'white'}} // add your styles to all item text of your list
-            // customChevron={{}} // your custom image to the indicator
-            // chevronColor="white" // color of the default indicator
-            //innerItemContainerStyle={{color:'white'}} // add your styles to all inner item containers of your list
-            //itemLabelStyle={{color:'white'}} // add your styles to all inner item text of your list
-            // itemImageIndicatorStyle={{}} // add your styles to the image indicator of your list
-            // animated={true} // sets all animations on/off, default on
-            // defaultLoaderStyles?: ViewStyle; // Set your styles to default loader (only for animated={true})
-            // customLoader?: JSX.Element; Pass your custom loader, while your content is measured and rendered (only for animated={true})
-            data={CONTENT} 
-            onInnerItemClick={handleInnerItemClick}
-            onItemClick={handleItemClick}
-        /> */}
+            <FontAwesome5 name="user-friends" size={30} color="black" />
+            <View style={{flexDirection: 'row'}}>
+                <Text style={{ fontSize: 30, color: 'black' }}>Friends List ({friends.length}) </Text> 
+                <Ionicons name="person-add" size={24} color="black" />
+            </View>
+            <Text style={{ fontSize: 18, color: 'white' }}> </Text> 
+            <Text style={{ fontSize: 18, color: 'white' }}>Remind your friends to stay healthy</Text>
+            <Text style={{ fontSize: 18, color: 'white' }}> </Text> 
+            <Text style={{ fontSize: 14, color: 'white' }}>Eg: Drink more water today!</Text>
+            <Text style={{ fontSize: 14, color: 'white' }}> </Text> 
+            <SafeAreaView>
+                <FlatList
+                    data={friends}
+                    renderItem={({item}) => (
+                        <View style={styles.listItem}>
+                            <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: "row" }}>
+                                <Text style={{  color: "#3b5998" }}> Friend: {item}</Text>
+                                <View style={{flexDirection: 'row'}}>
+                                    <FontAwesome.Button onPress={() => SMS.sendSMSAsync(
+                                        ['6504779097'],
+                                        'Drink some more water!',)}
+                                    style={{ backgroundColor: 'white', flexDirection: "row"}}>
+                                        <Ionicons name="chatbubble-ellipses-outline" size={20} color="black" />
+                                    </FontAwesome.Button>
+                                </View>
+                            </View>
+                        </View>
+                    )}
+                />
+            </SafeAreaView>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f8f8f8',
+      alignItems: 'center'
+    },
+    text: {
+      fontSize: 20,
+      color: '#101010',
+      marginTop: 60,
+      fontWeight: '700'
+    },
+    listItem: {
+      marginTop: 8,
+      padding: 8,
+      alignItems: 'center',
+      backgroundColor: 'white',
+      width: '100%'
+    },
+    listItemText: {
+      fontSize: 18
+    }
+  });
